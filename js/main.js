f82ddcb02ce6bc14de6187a215a331603f49b65c
Michelle H. McGrath <michelle.h.mcgrath@gmail.com>
2015-08-26 19:43:45 -0700

    static photos selected

--- js/main.js
+++ js/main.js
@@ -9,9 +9,21 @@
 
 for (i=0; i<12; i++) {
   var rando=Math.floor(Math.random()*11);
-  console.log(rando)
+  // console.log(rando)
 }
 
-$( document ).ready(function() {
+for (t=0; t<12; t++) {
+  var delay=Math.random()*2000;
+  // console.log(delay)
+}
+
+setTimeout(function(){
   $(".photo").toggleClass('flicker');
-  });
+}, delay);
+
+
+
+// $( document ).ready(function() {
+//   console.log(setTimeout());
+//
+//   });
